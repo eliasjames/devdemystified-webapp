d488ce61eee981cadfb311d2966d0fce2207b7cf
Elias Carlston <elias@eliascarlston.com>
2020-05-04 11:22:00 -0400

    Basic route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 const http = require('http');
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+  req.on('error', err => {
+    console.error(err);
+    res.statusCode = 400;
+    res.end('400: Bad Request');
+    return;
+  });
+
+  res.on('error', err => {
+    console.error(err);
+  });
+
+  const url = req.url;
+  if (url === "/route") {
+    res.writeHead(200);
+    res.end('Hello, Route!');
+  } else {
+    res.writeHead(200);
+    res.end('Hello, World!');
+  }
 }
 
 const server = http.createServer(requestListener);
