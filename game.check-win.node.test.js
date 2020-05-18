d7da5484531649da44b2becf00799f11fdaf15c0
Elias Carlston <elias@eliascarlston.com>
2020-05-18 15:00:34 -0400

    Check winner functions

---
+++ game.check-win.node.test.js
@@ -0,0 +1,47 @@
+const ttt = require("./game.core.node.js");
+
+const tests = [
+  {
+    label: "can check winner",
+    condition: () => { return ttt.checkWinner() !== undefined; },
+  },
+  {
+    label: "check no winner",
+    condition: () => { 
+      ttt.loadBoard([1, 1]);
+      return ttt.checkWinner() === 0; 
+    },
+  },
+  {
+    label: "check winner across",
+    condition: () => { 
+      const player = 1;
+      ttt.loadBoard([player, player, player]);
+      return ttt.checkWinner() === player; 
+    },
+  },
+  {
+    label: "check winner down",
+    condition: () => { 
+      const testBoard = [];
+      const player = 1;
+
+      testBoard[1] = player, testBoard[4] = player, testBoard[7] = player; 
+      ttt.loadBoard(testBoard);
+      return ttt.checkWinner() === player; 
+    },
+  },
+  {
+    label: "check winner diagonal",
+    condition: () => { 
+      const testBoard = [];
+      const player = 1;
+
+      testBoard[2] = player, testBoard[4] = player, testBoard[6] = player; 
+      ttt.loadBoard(testBoard);
+      return ttt.checkWinner() === player; 
+    },
+  },
+];
+
+module.exports = tests;
--- game.core.node.js
+++ game.core.node.js
@@ -1,9 +1,35 @@
+const validPlayers = [1, 2];
+
 module.exports = {
   board: [],
+  checkWinner: function checkWinner() {
+    const directions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let winState;
+
+    for (i = 0; i < directions.length; i++) {
+      let e = directions[i];
+      winState = (
+        this.board[e[0]] === this.board[e[1]]
+        && this.board[e[1]] === this.board[e[2]]
+        && this.board[e[0]] !== undefined
+      );
+      if (winState) return this.board[e[0]];
+    }
+    return 0;
+  },
   loadBoard: function loadBoard(board) {
     if (!(board instanceof Array)) throw new Error("board must be an array");
     board.forEach(e => {
-      if ([1, 2].indexOf(e) > -1) { return; }
+      if (validPlayers.indexOf(e) > -1) { return; }
       throw new Error("board values must be player numbers");
     });
     this.board = board;
