511fdc42fa6b251725bd576e5da50f974fbd7ce5
Elias Carlston <elias@eliascarlston.com>
2020-05-18 10:35:19 -0400

    Second test

--- game.core.node.test.js
+++ game.core.node.test.js
@@ -1,3 +1,7 @@
 const ttt = require("./game.core.node.js");
 
 console.log("ttt has a board", ttt.board !== undefined);
+console.log(
+  "ttt board is an array", 
+  (ttt.board instanceof Array) === true,
+);
