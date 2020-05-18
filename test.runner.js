c4eb46d230ae2b27d9458abbf668fb97b8c8c41a
Elias Carlston <elias@eliascarlston.com>
2020-05-18 12:30:35 -0400

    Refactor test runner for multiple files

--- test.runner.js
+++ test.runner.js
@@ -1,17 +1,24 @@
-const tttTests = require("./game.core.node.test.js");
+const coreTests = require("./game.core.node.test.js");
+const checkWinTests = require("./game.check-win.node.test.js");
+const tttTests = [
+  coreTests,
+  checkWinTests,
+];
 
-tttTests.forEach(e => {
-  const errorColor = "\x1b[31m%s\x1b[0m";
-  const dimOutput = "\x1b[2m%s\x1b[0m";
-  if (!e.condition()) {
+tttTests.forEach(eachTestGroup => {
+  eachTestGroup.forEach(e => {
+    const errorColor = "\x1b[31m%s\x1b[0m";
+    const dimOutput = "\x1b[2m%s\x1b[0m";
+    if (!e.condition()) {
+      console.log(
+        errorColor,
+        e.label,
+      );
+      return;
+    }
     console.log(
-      errorColor,
+      dimOutput,
       e.label,
     );
-    return;
-  }
-  console.log(
-    dimOutput,
-    e.label, 
-  );
+  });
 });
