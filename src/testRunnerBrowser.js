890c7a994b37e19cf3aa85f42241f7c40277e496
Elias Carlston <elias@eliascarlston.com>
2020-05-18 10:26:59 -0400

    Reset 20200518

--- src/testRunnerBrowser.js
+++
@@ -1,9 +0,0 @@
-const testRunner = {
-  testAndReport(testName, condition) {
-    if (condition) {
-      console.log(testName, condition);
-    } else {
-      alert(`${testName}: ${condition}`); 
-    }
-  },
-};
