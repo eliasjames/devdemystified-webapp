1eeb7e37471f9f258b3f780dd07e8a414c7672ba
Elias Carlston <elias@eliascarlston.com>
2020-05-18 10:56:46 -0400

    Improve output

--- test.runner.js
+++ test.runner.js
@@ -1,8 +1,17 @@
 const tttTests = require("./game.core.node.test.js");
 
 tttTests.forEach(e => {
+  const errorColor = "\x1b[31m%s\x1b[0m";
+  const dimOutput = "\x1b[2m%s\x1b[0m";
+  if (!e.condition()) {
+    console.log(
+      errorColor,
+      e.label,
+    );
+    return;
+  }
   console.log(
+    dimOutput,
     e.label, 
-    e.condition()
   );
 });
