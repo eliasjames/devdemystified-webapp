d04b3777b15b7d3d687fe40c6373df34caae69b0
Elias Carlston <elias@eliascarlston.com>
2020-06-24 16:26:03 -0400

    read index.html as home page

--- js/node.server.js
+++ js/node.server.js
@@ -1,8 +1,15 @@
+const fs = require("fs");
 const http = require("http");
 const gameFactory = require("./node.game.core");
 
 function serverFactory() {
   let game = undefined;
+  let homePage;
+
+  fs.readFile("index.html", (error, data) => {
+    if (error) console.log("error", error);
+    homePage = data;
+  });
 
   function requestListener (req, res) {
     const myResponse = myRouter(req.url);
@@ -17,7 +24,7 @@ function serverFactory() {
     if (url === "/") {
       response = {
         status: 200,
-        response: "Hello, World!",
+        response: homePage || "Home page not loaded",
       };
     } else if (url === "/newgame") {
       game = gameFactory();
